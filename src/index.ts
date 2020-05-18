de3a56d69b850eff8baecd4665fa432764869c46
mriise <mark.riise26@gmail.com>
2020-05-18 01:22:17 -0700

    fix dependancy issues

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import koa from 'koa'
 import {team, project} from './api'
 import client from './matrix'
+import { ConsoleLogger } from 'matrix-bot-sdk';
+// import { LogService, RichConsoleLogger, LogLevel } from 'matrix-bot-sdk';
 
 client
+
 var app = new koa();
 
 // app.use(team.routes());
 app.use(project.routes());
 app.listen(3000);
+// console.log('started')
 
-
+// LogService.setLogger(new RichConsoleLogger());
+// LogService.setLevel(LogLevel.DEBUG);
 
 // var MongoClient = require('mongodb').MongoClient;
 // var url = "mongodb://localhost:27017/mydb";
